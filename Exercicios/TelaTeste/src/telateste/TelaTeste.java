b175beb20103be73c159970d3f49f08b68febb15
Nodelay <121840258+Iraildo-Sousa@users.noreply.github.com>
2025-03-25 17:20:30 -0300

    Add files via upload

---
+++ Exercicios/TelaTeste/src/telateste/TelaTeste.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package telateste;
+
+/**
+ *
+ * @author Iraildo55882056
+ */
+public class TelaTeste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
