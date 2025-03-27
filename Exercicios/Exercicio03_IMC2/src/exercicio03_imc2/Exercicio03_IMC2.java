be7897de8f682f7577b580e110fead230a6d5bc5
Nodelay <121840258+Iraildo-Sousa@users.noreply.github.com>
2025-03-27 15:30:01 -0300

    Add files via upload

---
+++ Exercicios/Exercicio03_IMC2/src/exercicio03_imc2/Exercicio03_IMC2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio03_imc2;
+
+/**
+ *
+ * @author Iraildo55882056
+ */
+public class Exercicio03_IMC2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
